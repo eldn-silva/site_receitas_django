31ca497e2083ef04433a60c15bb1fe23d2628bab
Elder Nogueira <elder.nogueirasilva1@gmail.com>
2022-03-10 19:29:00 -0300

    Aplicando links, extends e partials

--- receitas/views.py
+++ receitas/views.py
@@ -3,4 +3,5 @@ from django.shortcuts import render
 def index(request):
     return render(request,'index.html')
 
-# Create your views here.
+def receita(request):
+    return render(request,'receita.html')
